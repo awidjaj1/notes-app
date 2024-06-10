b5fe47c2b37353b8def07fbb2c6c5178d34c42ed
Andrew Widjaja <awidjaj1@terpmail.umd.edu>
2024-06-10 16:48:32 -0400

    working on profile widget

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,17 @@
+import {
+    ManageAccountsOutlined,
+    EditOutlined,
+    LocaitonOnOutlined,
+    WorkOutlineOutlined
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserWidget = ({userId, picturePath}) => {
+    const [user, setUser] = useState(null);
+}
